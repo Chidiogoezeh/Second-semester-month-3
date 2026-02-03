66012f166737cdb6aa7f9657001efdaf1ca1ebc6
Chidiogo <chidiogoezeh.me@gmail.com>
2026-02-03 18:40:10 +0100

    updated auth.js to provide seperate routes for login and registration

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,11 @@ import * as authCtrl from '../controllers/authController.js';
 const router = express.Router();
 
 router.get('/login', authCtrl.renderLogin);
-router.post('/register', authCtrl.register);
 router.post('/login', authCtrl.login);
+
+router.get('/register', authCtrl.renderRegister);
+router.post('/register', authCtrl.register);
+
 router.get('/logout', authCtrl.logout);
 
 export default router;
